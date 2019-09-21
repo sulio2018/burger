91c364f2274bf1f83e1a634c338e7f75d3cb9a15
jmartinez <jcmartinez1925@gmail.com>
2019-09-20 22:02:22 -0700

    add api routes to controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,12 +5,37 @@ var burger = require("../models/burger.js");
 
 // Create routes
 router.get("/", function (req, res) {
-    burger.selectAll(function(data) {
-        var hbsObject = {
-            burgers: data
-        };
+    res.redirect("/burgers");
+});
 
+router.get("/burgers", function (req, res) {
+    burger.selectAll(function (data) {
+        var hbsObject = { burgers: data };
         res.render("index", hbsObject);
     });
 });
 
+router.post("/api/burgers/", function (req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, "0"
+    ], function (result) {
+        res.join({ id: result.insertId})
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = `id = ${req.params.id}`
+
+    burger.update(condition, function (result) {
+
+        if (result.changeRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
