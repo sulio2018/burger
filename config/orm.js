7a3e97f28d80232df09f793e73b7074f6be52710
jmartinez <jcmartinez1925@gmail.com>
2019-09-20 20:56:42 -0700

    set up orm

---
+++ config/orm.js
@@ -0,0 +1,42 @@
+var connection = require(".connection.js");
+
+module.exports = {
+    selectAll: function(input, cb) {
+
+        var queryString = `SELECT * FROM ${input};`;
+        connection.query(queryString, function(err, result) {
+
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+
+    insertOne: function(input, cols, vals, cb) {
+
+        var queryString = `INSERT INTO ${input} (${cols.toString()}) VALUES ("${vals[0]})", "${vals[1]}"`
+        connection.query(queryString, function (err, result) {
+
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+
+    updateOne: function(input, condition, cb) {
+
+        var queryString = `UPDATE ${input} SET devoured = true WHERE ${condition};`;
+        connection.query(queryString, function (err, result) {
+
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    }
+}
