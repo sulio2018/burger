213d047bc55c8eb15f20042b860aafee37cba406
jmartinez <jcmartinez1925@gmail.com>
2019-09-20 18:43:17 -0700

    set up server.js

--- server.js
+++ server.js
@@ -1,2 +1,31 @@
+// Require elements
 var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Read from the "public" folder
+app.use(express.static("public"));
+
+// Parse body as json
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// Set handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs ({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log(`Server Listening on http//localhost:${PORT}`);
+});
 
