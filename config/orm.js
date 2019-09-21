dbda21897fd24ff59c4bca20967fb423813ea5a7
jmartinez <jcmartinez1925@gmail.com>
2019-09-21 15:43:43 -0700

    redo orm with helper function

--- config/orm.js
+++ config/orm.js
@@ -1,42 +1,80 @@
-var connection = require("./connection.js");
+var connection = require("../connection.js");
 
-module.exports = {
-    selectAll: function(input, cb) {
-
-        var queryString = `SELECT * FROM ${input};`;
-        connection.query(queryString, function(err, result) {
-
-            if (err) {
-                throw err;
-            }
-
-            cb(result);
-        });
-    },
-
-    insertOne: function(input, cols, vals, cb) {
-
-        var queryString = `INSERT INTO ${input} (${cols.toString()}) VALUES ("${vals[0]})", "${vals[1]}"`
-        connection.query(queryString, function (err, result) {
-
-            if (err) {
-                throw err;
-            }
-
-            cb(result);
-        });
-    },
-
-    updateOne: function(input, condition, cb) {
-
-        var queryString = `UPDATE ${input} SET devoured = true WHERE ${condition};`;
-        connection.query(queryString, function (err, result) {
-
-            if (err) {
-                throw err;
-            }
-
-            cb(result);
-        });
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
     }
-}
+  
+    return arr.toString();
+  }
+  
+  // Helper function 
+  function objToSql(ob) {
+    var arr = [];
+  
+    for (var key in ob) {
+      if (Object.hasOwnProperty.call(ob, key)) {
+        arr.push(key + "=" + ob[key]);
+      }
+    }
+  
+    return arr.toString();
+  }
+  
+  
+  // Select all function
+  var orm = {
+    selectAll: function(tableInput, cb) {
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+  
+  // Insert function
+    insertOne: function(table, cols, vals, cb) {
+      var queryString = "INSERT INTO " + table;
+  
+      queryString += " (";
+      queryString += cols.toString();
+      queryString += ") ";
+      queryString += "VALUES (";
+      queryString += printQuestionMarks(vals.length);
+      queryString += ") ";
+  
+      console.log(queryString);
+  
+      connection.query(queryString, vals, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+  
+  // Update function
+    updateOne: function(table, objColVals, condition, cb) {
+      var queryString = "UPDATE " + table;
+  
+      queryString += " SET ";
+      queryString += objToSql(objColVals);
+      queryString += " WHERE ";
+      queryString += condition;
+  
+      console.log(queryString);
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    }
+  };
+  
+  module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -2,23 +2,23 @@ var orm = require("../config/orm.js");
 
 var burger = {
 
-    selectAll: function (cb) {
-        orm.selectAll("burgers", function (res) {
-            cb(res);
+      selectAll: function(cols, vals, cb) {
+        orm.selectAll("burgers", cols, vals, function(res) {
+          cb(res);
         });
-    },
-
-    insertOne: function (cols, vals, cb) {
-        orm.insertOne("burgers", cols, vals, function (res) {
-            cb(res);
+      },
+    
+      insertOne: function(objColVals, condition, cb) {
+        orm.insertOne("burgers", objColVals, condition, function(res) {
+          cb(res);
         });
-    },
-
-    updateOne: function (condition, cb) {
-        orm.updateOne("burgers", condition, function (res) {
-            cb(res);
+      },
+    
+      updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+          cb(res);
         });
-    }
-};
+      },
+    };
 
 module.exports = burger;
