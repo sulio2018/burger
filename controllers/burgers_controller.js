839d6fd528e01fbadc8a0221aef9aaf098742cfa
jmartinez <jcmartinez1925@gmail.com>
2019-09-21 15:50:48 -0700

    fix routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,36 +5,28 @@ var burger = require("../models/burger.js");
 
 // Create routes
 router.get("/", function (req, res) {
-    res.redirect("/burgers");
-});
-
-router.get("/burgers", function (req, res) {
     burger.selectAll(function (data) {
         var hbsObject = { burgers: data };
         res.render("index", hbsObject);
     });
 });
 
-router.post("/api/burgers/", function (req, res) {
-    burger.create([
-        "burger_name", "devoured"
-    ], [
-        req.body.burger_name, "0"
-    ], function (result) {
-        res.join({ id: result.insertId})
-    });
+router.post("/", function (req, res) {
+    burger.insertOne([
+        "burger_name"], [
+        req.body.name], function () {
+            res.redirect("/");
+
+        });
 });
 
-router.put("/api/burgers/:id", function (req, res) {
-    var condition = `id = ${req.params.id}`
+router.put("/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
 
-    burger.update(condition, function (result) {
-
-        if (result.changeRows == 0) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function() {
+        res.redirect("/");
     });
 });
 
