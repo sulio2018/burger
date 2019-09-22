ded5146df00c888bf8f79b48d73f70d28308adbf
jmartinez <jcmartinez1925@gmail.com>
2019-09-21 19:03:31 -0700

    edit connection

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ if (process.env.JAWSDB_URL) {
         host: 'localhost',
         user: 'root',
         password: 'julio',
-        database: 'buregers_db'
+        database: 'burgers_db'
     });
 };
 
