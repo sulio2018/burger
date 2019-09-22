908cc80099f192a1b74b12ea38f2ae35424bcd04
jmartinez <jcmartinez1925@gmail.com>
2019-09-21 19:01:51 -0700

    edit connection

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 
 var connection;
 
-if (precess.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(precess.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
